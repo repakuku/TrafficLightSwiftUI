b0c6f333e09cd37a3bd8bd6a7d8e9ceadb7c885f
Alexey Turulin <repakuku@icloud.com>
2023-07-19 14:25:47 -0400

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Алексей Турулин on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
